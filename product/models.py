0fe4aa940f2dea861882e40353f48f8cc13ee39e
Playdev <josang1204@gmail.com>
2019-12-04 18:53:50 +0900

    add product model

--- product/models.py
+++ product/models.py
@@ -6,10 +6,16 @@ class Product(models.Model):
     # TODO: Define fields here
     name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=15)
-
-
-
+    trading_place = models.TextField()
+    status = models.CharField(max_length=30)
+    price = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='img/', max_length=100)
+    
     def __str__(self):
         """Unicode representation of Product."""
-        pass
+        return self.name
+
+
+
+
 
