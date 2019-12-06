170103cadb71e8545713ea2e770f7dd13d3b9224
이규원 <48176412+joomint99@users.noreply.github.com>
2019-12-06 12:09:18 +0900

    Update forms.py
    product create fix

--- product/forms.py
+++ product/forms.py
@@ -3,8 +3,15 @@ from django import forms
 from django.utils import timezone
 
 
-class ProductForm(forms.ModelForm):         
-    end_time = forms.DateTimeField(label='옥션 기한', initial=timezone.localtime(), input_formats=['%Y/%m/%d %H:%M', ])
+class ProductForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['placeholder'] = "Name"
+        self.fields['phone_number'].widget.attrs['placeholder'] = "Phone Number"
+        self.fields['trading_place'].widget.attrs['placeholder'] = "Write Your Trading Place"
+        self.fields['price'].widget.attrs['placeholder'] = "Price"         
+        self.fields['end_time'] = forms.DateTimeField(label='옥션 기한', initial=timezone.localtime(), input_formats=['%Y/%m/%d %H:%M', ])
+
     class Meta:                                                                       
         model = Product
         fields = ['name', 'phone_number', 'trading_place', 'status', 'price', 'image']
